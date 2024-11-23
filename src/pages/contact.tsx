e88f3555f89b13da3b00d9bd37064e60a5140089
chancelle kabisa <chancellekabisa67@gmail.com>
2024-11-23 13:52:30 +0100

    creation  du composant navbar

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import  Navbar  from "../components/Navbar";
+import ImgOverlayExample from "../components/Cart"
+
+
+export default function Contact(){
+    return(
+        <div>
+            <Navbar/>
+            <ImgOverlayExample/>
+            
+        </div>
+    );
+}
